f29ed1801e28c8f9ff28f9228f9d7a4089a79c19
AyakorK <guillaume.moret@yahoo.com>
2024-03-22 10:28:59 +0100

    lint: Get rid of useless console logs

--- app/packs/src/decidim/extra_user_fields/signup_form.js
+++ app/packs/src/decidim/extra_user_fields/signup_form.js
@@ -9,10 +9,8 @@ $(document).ready(function() {
         underageCheckbox.on('change', function() {
             if (underageCheckbox.prop('checked')) {
                 statutoryRepresentativeEmailField.removeClass('hidden');
-                console.log("Underage checkbox checked.");
             } else {
                 statutoryRepresentativeEmailField.addClass('hidden');
-                console.log("Underage checkbox unchecked.");
             }
         });
     }
@@ -21,12 +19,9 @@ $(document).ready(function() {
         url: '/extra_user_fields/underage_limit', // Updated to match the new route
         type: 'GET',
         success: function(data) {
-            console.log("Underage limit fetched successfully.");
-            console.log(data);
             underageLimit = data.underage_limit;
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            console.log(jqXHR.responseText);
             console.error("Failed to fetch underage limit:", textStatus, errorThrown);
         }
     });
@@ -40,8 +35,6 @@ $(document).ready(function() {
             const currentDate = Date.now();
             const ageInMilliseconds = currentDate - dobDate;
             const age = Math.abs(new Date(ageInMilliseconds).getUTCFullYear() - 1970);
-
-            console.log(underageLimit);
 
             if (age < underageLimit) {
                 underageFieldSet.removeClass('hidden');
