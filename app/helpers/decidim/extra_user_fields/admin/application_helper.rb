a6a03353d06d5dee3cd116e368f4bdc324f3590d
AyakorK <guillaume.moret@yahoo.com>
2026-01-19 10:46:16 +0100

    feat: v1 of 0.31 bump EUF

--- app/helpers/decidim/extra_user_fields/admin/application_helper.rb
+++ app/helpers/decidim/extra_user_fields/admin/application_helper.rb
@@ -3,16 +3,15 @@
 module Decidim
   module ExtraUserFields
     module Admin
-      # Custom helpers, scoped to the meetings admin engine.
-      #
       module ApplicationHelper
         def extra_user_fields_export_users_dropdown
-          content_tag(:ul, class: "vertical menu add-components") do
+          content_tag(:ul, style: "list-style: none; margin: 0; padding: 0;") do
             Decidim::ExtraUserFields::AdminEngine::DEFAULT_EXPORT_FORMATS.map do |format|
-              content_tag(:li, class: "exports--format--#{format.downcase} export--users") do
+              content_tag(:li) do
                 link_to(
                   t("decidim.admin.exports.export_as", name: t("decidim.extra_user_fields.admin.exports.users"), export_format: format.upcase),
-                  AdminEngine.routes.url_helpers.extra_user_fields_export_users_path(format:)
+                  AdminEngine.routes.url_helpers.extra_user_fields_export_users_path(format:),
+                  style: "display: block; padding: 0.75rem 1rem; color: inherit; text-decoration: none; font-size: 0.875rem;"
                 )
               end
             end.join.html_safe
